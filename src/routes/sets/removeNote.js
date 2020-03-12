fe7a417ccc60455d56819b97ca5e799044d454c2
Nisheet Sinvhal <nisheet1.sinvhal@gmail.com>
2020-03-12 18:48:03 +0530

    Fix removeNote by changing the unset and pull key to notes

--- src/routes/sets/removeNote.js
+++ src/routes/sets/removeNote.js
@@ -2,13 +2,16 @@ const Sets = require("../../models/sets");
 
 module.exports = async ({ params }, res) => {
   console.log(params);
-  const { pageno, id } = params;
+  const { id } = params;
+  let { pageno } = params;
+  pageno = parseInt(pageno, 10);
   if (pageno >= 0 && id) {
     try {
-      await Sets.updateOne({ _id: id }, { $unset: { [`sets.${pageno}`]: 1 } });
-      await Sets.updateOne({ _id: id }, { $pull: { sets: null } });
+      await Sets.updateOne({ _id: id }, { $unset: { [`notes.${pageno}`]: 1 } });
+      await Sets.updateOne({ _id: id }, { $pull: { notes: null } });
       res.json(responseMessage.SUCCESS.SUCCESS);
     } catch (e) {
+      console.error(`removeNote: ${e}`);
       res.json(responseMessage.FAIL.SOMETHING_WRONG);
     }
   } else {
